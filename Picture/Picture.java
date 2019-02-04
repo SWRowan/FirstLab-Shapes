7d66e06e8399713e2e493cdfd796f92f675606e9
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-04 17:47:45 -0500

    finished the first lab

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -16,6 +16,12 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Square grass;
+    private Square Hobhouse;
+    private Square door;
+    private Square window1;
+    private Triangle tree;
+    private Square trunk;
 
     /**
      * Constructor for objects of class Picture
@@ -33,26 +39,87 @@ public class Picture
         wall = new Square();
         wall.moveVertical(80);
         wall.changeSize(100);
-        wall.makeVisible();
+        
 
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
-        window.moveVertical(100);
-        window.makeVisible();
+        
+        trunk = new Square();
+        trunk.changeColor("black");
+        trunk.changeSize(40);
+        trunk.moveVertical(260);
+        trunk.moveHorizontal(325);
+        trunk.makeVisible();
+        
+        tree = new Triangle();
+        tree.changeColor("green");
+        tree.changeSize(200, 80);
+        tree.moveHorizontal(350);
+        tree.moveVertical(100);
+        tree.makeVisible();
+        
+     
 
-        roof = new Triangle();
-        roof.changeSize(50, 140);
-        roof.moveHorizontal(60);
-        roof.moveVertical(70);
-        roof.makeVisible();
+        
 
         sun = new Circle();
         sun.changeColor("yellow");
-        sun.moveHorizontal(180);
-        sun.moveVertical(-10);
-        sun.changeSize(60);
+        sun.moveHorizontal(400);
+        sun.moveVertical(-150);
+        sun.changeSize(150);
         sun.makeVisible();
+        
+        
+       
+      
+        
+        Hobhouse = new Square();
+        Hobhouse.changeColor("black");
+        Hobhouse.changeSize(250);
+        Hobhouse.moveVertical(200);
+        Hobhouse.makeVisible();
+        
+        
+        roof = new Triangle();
+        roof.changeColor("red");
+        roof.changeSize(50, 300);
+        roof.moveHorizontal(135);
+        roof.moveVertical(190);
+        roof.makeVisible();
+        
+        
+        door = new Square();
+        door.changeColor("yellow");
+        door.changeSize(50);
+        door.moveVertical(255);
+        door.moveHorizontal(100);
+        door.makeVisible();
+        
+        window = new Square();
+        window.changeColor("white");
+        window.moveHorizontal(30);
+        window.moveVertical(250);
+        window.makeVisible();
+        
+        window1 = new Square();
+        window1.changeColor("white");
+        window1.moveHorizontal(185);
+        window1.moveVertical(250);
+        window1.makeVisible();
+        
+        
+        grass = new Square();
+        grass.changeColor("green");
+        grass.changeSize(600);
+        grass.moveVertical(300);
+        grass.moveHorizontal(-60);
+        grass.makeVisible();
+        
+        
+        
+        
+        
+       
+        
+        
     }
 
     /**
